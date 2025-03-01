e70bcbf89ad2c5a8272cfa63c5e2de177107e063
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-03-01 16:03:39 +0200

    add graphql starpi

--- packages/cms/config/plugins.ts
+++ packages/cms/config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default {
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
+};
